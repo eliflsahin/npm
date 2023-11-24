c718db42a51a1e13ccef8e39a9ae37099ab53fe6
eliflsahin <74318506+eliflsahin@users.noreply.github.com>
2023-11-23 23:21:55 -0500

    added githublink and release

--- harvard_census/harvard_census_to_csv.py
+++ harvard_census/harvard_census_to_csv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import csv
-from harvard_functions import get_latest_version, get_latest_version_dependencies, is_dependency_signed
+from harvard_functions import get_latest_version, get_latest_version_dependencies, is_dependency_signed, get_github_link, get_github_link_from_npm_page, has_github_release
 
 # Load the Excel file
 excel_file = 'harvard_census_top_100.xlsx'
@@ -12,11 +12,13 @@ with open(output_file, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     # Write the header row
-    csvwriter.writerow(["Package", "Version", "Dependency", "Dependency Version", "Relationship", "Signed/Not Signed"])
+    csvwriter.writerow(["Package", "Version", "Dependency", "Dependency Version", "Relationship", "Signed/Not Signed", "GithubLink", "Release"])
 
     # Iterate through the package names from the second column (package name)
     for package in df.iloc[:, 1]:
         latest_version = get_latest_version(package)
+        github_link = get_github_link(package)
+        github_has_release = has_github_release(github_link)
         
         if latest_version:
             version = latest_version.get('versionKey', {}).get('version', 'null')
@@ -30,17 +32,17 @@ with open(output_file, 'w', newline='') as csvfile:
                         dependency_name = dependency_info.get('versionKey', {}).get('name', 'unknown')
                         dependency_version = dependency_info.get('versionKey', {}).get('version', 'unknown')
                         signed_status = is_dependency_signed(package, dependency_name, dependency_version)
-                        csvwriter.writerow([package, version, dependency_name, dependency_version, relation, signed_status])
+                        csvwriter.writerow([package, version, dependency_name, dependency_version, relation, signed_status, github_link, int(github_has_release)])
                         has_dependencies = True
                 if not has_dependencies:
                     # Write a row for the package with "no dependency"
-                    csvwriter.writerow([package, version, None, None, None, None])
+                    csvwriter.writerow([package, version, None, None, None, None, github_link, int(github_has_release)])
             else:
                 # Write a row for the package with "no dependency"
-                csvwriter.writerow([package, version, None, None, None, None])
+                csvwriter.writerow([package, version, None, None, None, None, github_link, int(github_has_release)])
         else:
             # Write a row for the package that doesn't exist
-            csvwriter.writerow([package, None, None, None, None, None])
+            csvwriter.writerow([package, None, None, None, None, None, github_link, int(github_has_release)])
 
-# Output = package, latest version, dependency, dependency version, relationship, signed/ornot
+# Output = package, latest version, dependency, dependency version, relationship, signed/ornot, GithubLink, Release
 print(f"Package dependencies have been exported to {output_file}")
